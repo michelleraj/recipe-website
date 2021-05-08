4968a9365fb2bdbdf79899d7313a0082df1fd144
Helena Raj <helenaraj@Helenas-MacBook-Air.local>
2021-05-07 21:27:44 -0400

    my recipe page

--- src/server.js
+++ src/server.js
@@ -3,6 +3,22 @@ import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 import path from 'path';
 
+[
+    {
+        name: 'recipe1',
+        upvotes: 0,
+        comments: [],
+    }, {
+        name: 'recipe2',
+        upvotes: 0,
+        comments: [],
+    }, {
+        name: 'recipe3',
+        upvotes: 0,
+        comments: [],
+    },
+]
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/build')));
@@ -11,7 +27,7 @@ app.use(bodyParser.json());
 const withDB = async (operations, res) => {
     try {
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('my-blog');
+        const db = client.db('healthy-recipes');
     
         await operations(db);
     
@@ -21,43 +37,43 @@ const withDB = async (operations, res) => {
     }
 }
 
-app.get('/api/articles/:name', async (req, res) => {
+app.get('/api/recipe/:name', async (req, res) => {
     withDB(async (db) => {
         const articleName = req.params.name;
 
-        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        const articleInfo = await db.collection('recipe').findOne({ name: articleName })
         res.status(200).json(articleInfo);
     }, res);
 })
 
-app.post('/api/articles/:name/upvote', async (req, res) => {
+app.post('/api/recipe/:name/upvote', async (req, res) => {
     withDB(async (db) => {
         const articleName = req.params.name;
     
-        const articleInfo = await db.collection('articles').findOne({ name: articleName });
-        await db.collection('articles').updateOne({ name: articleName }, {
+        const articleInfo = await db.collection('recipe').findOne({ name: articleName });
+        await db.collection('recipe').updateOne({ name: articleName }, {
             '$set': {
                 upvotes: articleInfo.upvotes + 1,
             },
         });
-        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        const updatedArticleInfo = await db.collection('recipe').findOne({ name: articleName });
     
         res.status(200).json(updatedArticleInfo);
     }, res);
 });
 
-app.post('/api/articles/:name/add-comment', (req, res) => {
+app.post('/api/recipe/:name/add-comment', (req, res) => {
     const { username, text } = req.body;
     const articleName = req.params.name;
 
     withDB(async (db) => {
-        const articleInfo = await db.collection('articles').findOne({ name: articleName });
-        await db.collection('articles').updateOne({ name: articleName }, {
+        const articleInfo = await db.collection('recipe').findOne({ name: articleName });
+        await db.collection('recipe').updateOne({ name: articleName }, {
             '$set': {
                 comments: articleInfo.comments.concat({ username, text }),
             },
         });
-        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        const updatedArticleInfo = await db.collection('recipe').findOne({ name: articleName });
 
         res.status(200).json(updatedArticleInfo);
     }, res);
